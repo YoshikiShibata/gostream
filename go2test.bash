7f859e262e1ae5a4286defb966fcbf79ae054a97
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2020-07-05 17:10:09 +0900

    updated

--- go2test.bash
+++ go2test.bash
@@ -2,7 +2,11 @@
 
 source ~/bin/switch_go2.bash
 
-for go2 in generic.go2 map.go2 slice.go2 stream_test.go2; 
+for go2 in generic.go2 map.go2 slice.go2 \
+		generic_test.go2 \
+		map_test.go2 \
+		slice_test.go2 \
+		stream_test.go2; 
 do
 	echo "formatting $go2 ..."
 	gofmt -w $go2
